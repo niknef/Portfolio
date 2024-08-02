d84b3772f275bc338bfb8c47941ce6edc496b6cf
NikNef <nicolas.firpo@davinci.edu.ar>
2024-08-02 18:51:44 -0300

    add resend 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  site: "https://nicolasfirpo.vercel.app/"
+  adapters: [vercel()],
 });
