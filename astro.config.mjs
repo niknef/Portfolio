537446b0baae1d173c25acdfce68f88a070bde15
NikNef <nicolas.firpo@davinci.edu.ar>
2024-08-02 19:31:02 -0300

    fix error tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-
 import { defineConfig } from 'astro/config';
 // Also can be @astrojs/vercel/static
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
- 
+
+// https://astro.build/config
 export default defineConfig({
   // Also can be 'static' or 'hybrid'
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    maxDuration: 8,
+    maxDuration: 8
   }),
+  integrations: [tailwind()]
 });
